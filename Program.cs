c92160e9c73baaf3d2118c8c3bb54de3cd7c2a8d
developer-onizuka <developer@example.com>
2023-06-11 13:20:12 +0000

    2023-06-10

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class test
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Start Program");
+
+        Console.WriteLine("Run Child process");
+        Task childAsync = MethodAsync();
+
+        for (int i = 1; i <= 2; i++) 
+        {
+            GetThreadId($"Main Heavy process #{i} *");
+            Task.Delay(1000).Wait();
+        }
+        Console.WriteLine("All of Main Heavy processes completed");
+
+        await childAsync;
+        Console.WriteLine("Confirm all of child processes completed after Main processes finished");
+    }
+
+    static async Task MethodAsync()
+    {
+        for (int i = 1; i <= 10; i++) 
+        {
+            GetThreadId($"ChildAsync process #{i}");
+            await Task.Delay(300);
+        }
+    }
+
+    static void GetThreadId(string str) 
+    {
+        int threadId = Thread.CurrentThread.ManagedThreadId;
+        Console.WriteLine($"Thread {threadId} -----> {str}");
+    }
+}
